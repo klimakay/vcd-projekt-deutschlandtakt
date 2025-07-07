486bb5d48be084c2ab15c48666d65bf3db819f52
bohong <bohongli2001@gmail.com>
2025-07-07 18:18:16 +0200

    added the module description providing the purpose of this program, as it was initially missing or forgotten

--- src/vcd_projekt_deutschlandtakt/auswertung.py
+++ src/vcd_projekt_deutschlandtakt/auswertung.py
@@ -1,3 +1,11 @@
+"""
+    This module evaluates the primary parameters of the train schedules, both pre- and post-Deutschlandtakt, which are:
+    travel time (Reisezeit), the pure travel speed with the train (Befoerderungsgeschwindigkeit), comfort (Komfort),
+    and train freuency (Taktfrequenz).
+
+    Parameters concerning transits are ignored, as in this case we evaluate direct transits between two destinations.
+"""
+
 import pandas as pd
 
 def read_all_data(filename: str) -> tuple[dict[str, pd.DataFrame], list[int | str]]:
